dc9dd9bc06e5aad6f07dc2622a0ca7d807bcbe7d
David LLuna <llunaplanet@gmail.com>
2017-03-25 00:44:28 +0100

    Keep testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,19 @@ resource "aws_api_gateway_rest_api" "rest-api" {
   name = "slackjukebox Endpoint"
   description = "Endpoint for HTTPS Slack webhook"
 }
+
+resource "aws_dynamodb_table" "slackjukebox" {
+  name           = "slackjukebox"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "PlayListId"
+
+  attribute {
+    name = "PlayListId"
+    type = "S"
+  }
+
+  tags {
+    Name        = "slackjukebox"
+  }
+}
