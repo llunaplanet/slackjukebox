47fdbfeaf6925e5e511e83813093efd7da533277
David LLuna <llunaplanet@gmail.com>
2017-03-25 00:01:30 +0100

    Adds terraform s3 configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,12 @@
-resource "aws_lambda_function" "test_lambda" {
-  filename         = "lambda_function_payload.zip"
-  function_name    = "lambda_function_name"
-  role             = "${aws_iam_role.iam_for_lambda.arn}"
-  handler          = "exports.test"
-  source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-
-  environment {
-    variables = {
-      foo = "bar"
+terraform {
+    backend "s3" {
+        bucket = "slackjukebox"
+        key    = "terraform.tfstate"
+        region = "eu-west-1"
     }
-  }
+}
+
+resource "aws_iam_user" "slackjukebox" {
+  name = "slackjukebox"
+  path = "/"
 }
