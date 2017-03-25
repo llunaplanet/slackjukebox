ae77cc7a2d9f8cbafcbfc71d400de5586841de5b
David LLuna <llunaplanet@gmail.com>
2017-03-25 16:11:47 +0100

    WIP

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,36 +1,30 @@
-resource "aws_iam_role" "iam_role_for_lambda" {
-    name = "iam_role_for_slackjukebox_lambda"
-    assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": {
-        "Effect": "Allow",
-        "Principal": {"Service": "lambda.amazonaws.com"},
-        "Action": "sts:AssumeRole"
-      }
+data "aws_iam_policy_document" "aws_iam_role" {
+
+  statement {
+    effect = "allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+        type = "Service"
+        identifiers = ["lambda.amazonaws.com"]
     }
-    EOF
+  }
+
+  statement {
+    effect = "allow"
+    actions = [
+        "cloudwatch:*",
+        "events:*",
+        "lambda:*",
+        "logs:*",
+        "s3:*"
+    ]
+    resources = [
+      "*",
+    ]
+  }
 }
 
-resource "aws_iam_role_policy" "lambda_exec_role" {
-    name = "lambda_exec_role"
-    role = "${aws_iam_role.iam_role_for_lambda.id}"
-    policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Effect": "Allow",
-          "Action": [
-            "cloudwatch:*",
-            "events:*",
-            "lambda:*",
-            "logs:*",
-            "s3:*"
-          ],
-          "Resource": "*"
-        }
-      ]
-    }
-    EOF
+resource "aws_iam_role" "iam_role_for_lambda" {
+    name = "iam_role_for_slackjukebox_lambda"
+    assume_role_policy = "${aws_iam_policy_document.aws_iam_role.json}"
 }
